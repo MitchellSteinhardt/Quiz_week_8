c7d616b7a47951e5e9d212385712d01bc4628a08
MitchellSteinhard <m.steinhardt@outlook.com>
2023-09-11 21:47:33 +1000

    climate_q1 v1.0

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,36 @@
 import matplotlib.pyplot as plt
-        
+
+# Importing sqlite3
+import sqlite3
+
+# Moved Tables above database querying
 years = []
 co2 = []
 temp = []
+
+# Connection to db, cursor and query created
+connection = sqlite3.connect('climate.db')
+cursor = connection.cursor()
+query = "SELECT Year, co2, Temperature FROM ClimateData"
+cursor.execute(query)
+
+
+# For each row at the cursor's fetchall, append to respective list
+for row in cursor.fetchall():
+    years.append(row[0])
+    co2.append(row[1])
+    temp.append(row[2])
+
+cursor.close()
+connection.close()
+
+
+# Printing lists for clarity
+print("Year data:", years)
+print("co2 data:", co2)
+print("Temp data:", temp)
+        
+
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
